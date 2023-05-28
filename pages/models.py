c6607974e8840ab500b1deb74513c8ee6c5f8c70
Manar Magdi <maanaarmagdii@gmail.com>
2023-05-29 01:17:47 +0300

    x1

---
+++ pages/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Create your models here.
+
+    
+    
+    
+      
+class Signup(models.Model):
+    Email= models.CharField(max_length=100, null= True)
+    Phone= models.CharField(max_length=100, null= True)
+    Address= models.CharField(max_length=100, null= True)
+    Password= models.CharField(max_length=100, null= True)
+    Username= models.CharField(max_length=100 , null= True)
+    Image = models.ImageField(upload_to="media/"  , null= True) 
+    
+    def __str__(self):
+        return self.Email 
+
+
+
+class Product1(models.Model):
+    name = models.CharField(max_length=60)
+    material = models.CharField(max_length=50)
+    fabric_finish = models.CharField(max_length=50)
+    size = models.CharField(max_length=50)
+    characteristic = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to="media/")
+    def re(self):
+         return reverse('add', args=[self.id])
+    
+    def __str__(self):
+        return self.name 
+
+   #btkhlyy kol product ba3mlu add yezhr fel database be esmuu 
+    
+    class Meta:
+        verbose_name= 'scarf'
+        
+   #btghyrly el esm el ra'esyyy lel table fel database -> (ADD "Scarf")
+   
+   
+class Product2(models.Model):
+    name= models.CharField(max_length=60)
+    color= models.CharField(max_length=60)
+    material= models.CharField(max_length=60)
+    price=models.DecimalField(max_digits=6, decimal_places=2)
+    Length_from_shoulder_to_feet= models.CharField(max_length=60)
+    image=models.ImageField(upload_to="media/")
+    
+    def __str__(self):
+        return self.name 
+    
+    class Meta:
+        verbose_name= 'Prayer dress'
+        
+class Cart(models.Model):
+     user = models.OneToOneField(User, on_delete=models.CASCADE)
+     products = models.ManyToManyField(Product1)
+     
+    
---
+++ pages/views.py
@@ -0,0 +1,115 @@
+from django.shortcuts import render, redirect 
+from .models import Signup, Product1, Product2 , Cart
+from .forms import LogInForm 
+from django.contrib.auth.decorators import login_required 
+from django.core.exceptions import ObjectDoesNotExist
+
+
+# Create your views here.
+
+
+def Home(request):
+    return render(request, "pages/home.html")
+
+
+def Shopnow(request):
+    scarfs = Product1.objects.all()
+    cart = Cart.objects.all()
+    # print(scarfs[0])
+    context = {"scarfs": scarfs , "cart": cart}
+    return render(request, "pages/shopnow.html", context)
+
+
+# def Login(request):
+#     return render(request, "pages/login.html")
+def Login(request):
+    if request.method == "POST":
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            Email = form.cleaned_data["Email"]
+            Password = form.cleaned_data["Password"]
+            user = Signup.objects.filter(Email=Email, Password=Password).first()
+            if user:
+                # set user session
+                request.session["user_id"] = user.id
+                return redirect("hali")
+            else:
+                # invalid login
+                return render(
+                    request,
+                    "pages/login.html",
+                    {"form": form, "error": "Invalid login credentials."},
+                )
+    else:
+        form = LogInForm()
+        return render(request, "pages/login.html", {"form": form})
+
+
+def signup(request):
+    email = request.POST.get("email")
+    phone = request.POST.get("phone")
+    address = request.POST.get("address")
+    password = request.POST.get("password")
+    username = request.POST.get("username")
+    data = Signup(
+        Email=email, Phone=phone, Address=address, Password=password, Username=username
+    )
+    data.save()
+    return render(request, "pages/signup.html")     
+
+
+def cart(request):
+       # Get the user's cart and all products associated with it
+    try:
+        cart = Cart.objects.get(user=request.user)
+        products = cart.products.all()
+        return render(request, "pages/cart.html" , {'cart': cart, 'products': products})
+    except : # Cart.ObjectDoesNotExist:
+        cart = None
+        products = []
+
+    # Render the cart template with the user's cart and products
+   
+        return render(request, "pages/cart.html" , {'cart': cart, 'products': products})
+
+
+def Pro(request):
+    user_id = request.session.get("user_id")
+    user = Signup.objects.get(id=user_id)
+
+    return render(request, "pages/pro.html", {"user": user})
+
+
+def Hijab(request):
+    prayer = Product2.objects.all()
+    # print(scarfs[0])
+    dresses = {"prayer": prayer}
+    return render(request, "pages/hijab.html" , dresses)
+
+
+def HALI(request):
+    return render(request, "pages/hali.html")
+
+def Add_to_cart(request, product_id):
+    # Get the product object by its ID
+    product = Product1.objects.get(id=product_id)
+
+    # Check if the user has a cart
+    try:
+        cart = Cart.objects.get(user=request.user)
+        cart.products.add(product)
+
+         # Redirect the user to the cart page
+        return redirect('cart')
+    except :#Cart.ObjectDoesNotExist:
+        # If the user doesn't have a cart, create one
+        cart = Cart.objects.create(user=request.user)
+        cart.products.add(product)
+
+    # Redirect the user to the cart page
+        return redirect('cart')
+    # Add the product to the user's cart
+    #cart.products.add(product)
+
+    # Redirect the user to the cart page
+    #return redirect('cart')
